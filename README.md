3ab3ac50ebf4400ac083408cef32766ca0c07e20
shu1007 <shuu.na1007@icloud.com>
2020-04-14 01:15:22 +0900

    初回コミット

---
+++ README.md
@@ -0,0 +1,11 @@
+# discord-sort-channels
+
+## Discription
+
+指定したカテゴリーのチャンネルを辞書式順序で並び替えます。
+
+## Command
+
+`!sort <Category Name> [desc]`
+
+カテゴリー名のあとに desc を付与すると降順に並び替えます。
---
+++ bot.js
@@ -0,0 +1,74 @@
+const Discord = require("discord.js");
+require("dotenv").config();
+const client = new Discord.Client();
+
+const sortChannels = async (channelArray, isDesc) => {
+  const hoge = isDesc ? -1 : 1;
+  channelArray.sort((a, b) => (a.name >= b.name ? hoge : -hoge));
+  for (let i = 0; i < channelArray.length; i++) {
+    const ch = channelArray[i];
+    console.log(`${ch.name}, position:${ch.position}, i:${i}`);
+    if (ch.position !== i)
+      await ch
+        .setPosition(i)
+        .then((newChannel) =>
+          console.log(
+            `${newChannel.name} new position is ${newChannel.position}`
+          )
+        );
+  }
+};
+
+client.on("ready", () => {
+  console.log("client ready...");
+});
+
+const COMMAND_PREFIX = "!sort";
+client.on("message", async (message) => {
+  if (message.channel.type !== "text") return;
+
+  const content = message.content;
+  if (!content.startsWith(COMMAND_PREFIX)) return;
+
+  const args = content.split(" ").slice(1);
+  const channel = message.channel;
+  const sendMessage = (m) => channel.send(m).catch(console.err);
+
+  if (args.length === 0) {
+    sendMessage("Please specify category.");
+    return;
+  }
+
+  const categoryName = args[0];
+  const guildId = channel.guild.id;
+  const guildChannels = client.channels.cache
+    .array()
+    .filter((ch) => ch.guild.id === guildId);
+  const categoryIds = guildChannels
+    .filter(
+      (ch) =>
+        ch.guild.id === guildId &&
+        ch.type === "category" &&
+        ch.name === categoryName
+    )
+    .map((cate) => cate.id);
+
+  if (categoryIds.length === 0) {
+    sendMessage("No targets.");
+    return;
+  }
+
+  const isDesc = args[1] !== undefined && args[1].toLowerCase() === "desc";
+  categoryIds.forEach(async (id) => {
+    sortChannels(
+      guildChannels.filter((ch) => ch.type === "text" && ch.parentID === id),
+      isDesc
+    );
+    sortChannels(
+      guildChannels.filter((ch) => ch.type === "voice" && ch.parentID === id),
+      isDesc
+    );
+  });
+});
+
+client.login(process.env.TOKEN);
